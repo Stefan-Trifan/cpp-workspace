21996cb9d267c65f7c6f2ddc50a2c62ff47770ac
Stefan <trfstephan@gmail.com>
2026-02-25 15:38:26 +0100

    up

--- 02_utad_evaluables/02b_algo_orden_insercion/main.cpp
+++ 02_utad_evaluables/02b_algo_orden_insercion/main.cpp
@@ -15,11 +15,40 @@ int main()
 	ordenarInsercion(x, n);
 }
 
+/**
+ * @brief Algoritmo de ordenación insercion
+ *
+ * @param x: Array de numeros a ordenar
+ * @param len: Longitud del array
+ *
+ * Recorremos el array. Vamos comparando cada elemento con su anterior
+ * Si vemos que el elemento posterior es mayor que el anterior:
+ * - guardamos la posicion del mayor
+ * - lo comparamos con todos los elementos anteriores, hasta que encontremos
+ * uno que sea menor o igual
+ * - a medida que lo vamos comparando los movemos una posicion mas arriba
+ * - cuando encontramos un elemento menor, insertamos el elemento guardado
+ *
+ */
 void ordenarInsercion(int x[], int len)
 {
     for (int i = 1; i < len; i++)
     {
         /*Añadir código de ordenación mediante inserción*/
+        if(x[i - 1] > x[i])
+        {
+            int temp = x[i];
+            int j = i - 1;
+
+            while(x[j] > temp)
+            {
+                x[j + 1] = x[j];
+                j--;
+            }
+
+            x[j + 1] = temp;
+        }
+
 
         for (int k = 0; k < len; k++)
         {

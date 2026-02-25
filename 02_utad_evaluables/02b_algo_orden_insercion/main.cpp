e94808d0ae965fa5c496f80708dffbb99a19f582
Stefan <trfstephan@gmail.com>
2026-02-25 10:27:50 +0100

    update

---
+++ 02_utad_evaluables/02b_algo_orden_insercion/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void ordenarInsercion(int x[], int len);
+
+int main()
+{
+    int n;
+	cin >> n;
+	int *x = new int[n];
+	for (int i = 0; i < n; i++)
+    cin >> x[i];
+	ordenarInsercion(x, n);
+}
+
+void ordenarInsercion(int x[], int len)
+{
+    for (int i = 1; i < len; i++)
+    {
+        /*Añadir código de ordenación mediante inserción*/
+
+        for (int k = 0; k < len; k++)
+            cout << x[k] << " ";
+        cout << endl;
+    }
+}

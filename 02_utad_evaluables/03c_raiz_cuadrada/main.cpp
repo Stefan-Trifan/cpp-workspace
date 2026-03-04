1e155a051acc8ecfe31aebf4ecc368b107151218
Stefan <trfstephan@gmail.com>
2026-03-04 23:27:49 +0100

    up

--- 02_utad_evaluables/03c_raiz_cuadrada/main.cpp
+++ 02_utad_evaluables/03c_raiz_cuadrada/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #define assertdomjudge(x) if(!(x)){std::cout<<"ERROR"<<std::endl;exit(0);}
 
 // Funciones del programa
-float calcular_raiz(float temp, float num, float err);
+float calcular_raiz(float mitad, float num, float err, float min, float max);
 
 /* _________________________________________
    Inicio main() */
@@ -27,7 +27,7 @@ int main()
     cin >> mi_num;
     cin >> mi_err;
 
-    cout << calcular_raiz(mi_num, mi_num, mi_err) << '\n';
+    cout << calcular_raiz(mi_num / 2, mi_num, mi_err, 0, mi_num) << '\n';
 
 	return 0;
 }
@@ -36,10 +36,8 @@ int main()
    Inicio definicion de funciones */
 
 // Funciones del programa
-float calcular_raiz(float temp, float num, float err)
+float calcular_raiz(float mitad, float num, float err, float min, float max)
 {
-    float mitad = temp / 2; // [mitad] = 7.5,[temp] = 15, [num] = 15
-
     printf("Vuelta [mitad]: %.3f\n", mitad);
     // Caso base
     if(mitad * mitad >= num - err && mitad * mitad <= num + err)
@@ -51,12 +49,16 @@ float calcular_raiz(float temp, float num, float err)
     // Reduccion del problema + Llamada recursiva
     if(mitad * mitad > num)
     {
-        // printf("DEBUG reduccion > [mitad]: %.3f\n", mitad);
-        return calcular_raiz(mitad, num, err);
+        printf("reduccion\n");
+        min = mitad / 2;
+        max = mitad;
+        return calcular_raiz((max - mitad) / 2, num, err, min, max);
     }
-    else
+    else if(mitad * mitad < num)
     {
-        // printf("DEBUG reduccion < [mitad]: %.3f\n", mitad);
-        return calcular_raiz(mitad + mitad / 2, num, err);
+        printf("ampliacion\n");
+        min = mitad / 2;
+        max = mitad;
+        return calcular_raiz(mitad + mitad / 2, num, err, min, max);
     }
 }

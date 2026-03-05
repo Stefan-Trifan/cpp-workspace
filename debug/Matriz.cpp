8e9ba04e46a0251234b624398d33cb9de0b3a107
Stefan <trfstephan@gmail.com>
2026-03-05 12:53:33 +0100

    update

---
+++ debug/Matriz.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Stephan Trf on 5/3/26.
+//
+
+#include "Matriz.h"
---
+++ debug/Matriz.h
@@ -0,0 +1,16 @@
+//
+// Created by Stephan Trf on 5/3/26.
+//
+
+#ifndef MATRIZ_H
+#define MATRIZ_H
+
+
+
+class Matriz {
+
+};
+
+
+
+#endif //MATRIZ_H

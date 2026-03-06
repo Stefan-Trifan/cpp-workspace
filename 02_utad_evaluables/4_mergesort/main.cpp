b870b10dcf42dbd2010bdffe0d94b86071718ce2
Stefan <trfstephan@gmail.com>
2026-03-06 15:19:08 +0100

    merge sort

--- 02_utad_evaluables/4_mergesort/main.cpp
+++ 02_utad_evaluables/4_mergesort/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
 void combinar(int a[], int len_a, int b[], int len_b, int c[]);
-void ordenarMergeSort(int x[], int len);
+void MergeSort(int x[], int len);
 
 int main()
 {
@@ -13,24 +15,49 @@ int main()
     {
 		cin >> x[i];
     }
-	ordenarMergeSort(x, n);
+	MergeSort(x, n);
 }
-void ordenarMergeSort(int x[], int len)
+
+void MergeSort(int x[], int len)
 {
 	int temp[len];
 
-	if (len == 2) // caso básico
+	if (len == 1) // caso base
 	{
 		return;
 	}
 	else
 	{
-		// Ordenar dos mitades
+        // Instanciar 2 mitades
+        int l_len = len / 2;
+        int r_len = len - len / 2;
 
-		// Combinar ambas mitades y guardarlo en temp
+        int left[l_len];
+        int right[r_len];
+
+        // Dividir en 2 mitades
+        int i;
+        for(i = 0; i < l_len; i++)
+        {
+            left[i] = x[i];
+            // printf("<- %d\n", left[i]);
+        }
+        for(int j = 0; j < r_len; j++)
+        {
+            right[j] = x[i];
+            i++;
+            // printf("-> %d\n", right[j]);
+        }
+
+		// Ordenar dos mitades
+        MergeSort(left, l_len);
+        MergeSort(right, r_len);
+
+        // Combinar ambas mitades y guardarlo dentro de temp
+        combinar(left, l_len, right, r_len, temp);
 
 		// Copiar del array temporal al array resultado
-		for (int k = 0; k < len; k++)
+        for (int k = 0; k < len; k++)
 		{
 			x[k] = temp[k];
 			cout << x[k] << " ";

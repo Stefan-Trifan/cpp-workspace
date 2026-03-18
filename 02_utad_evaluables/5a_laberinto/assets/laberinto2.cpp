cfce17890948bb8006ff25c9cc85e6dcbea7215f
Stefan <trfstephan@gmail.com>
2026-03-18 12:17:11 +0100

    update

--- 02_utad_evaluables/5a_laberinto/assets/laberinto2.cpp
+++ 02_utad_evaluables/5a_laberinto/assets/laberinto2.cpp
@@ -9,8 +9,11 @@
    Inicio cabecera */
 
 #include <iostream>
+// clang-format off
+#define assertdomjudge(x) if(!(x)){std::cout<<"ERROR"<<std::endl;exit(0);}
+// clang-format on
 
-#define TAM 10
+#define TAM 60
 #define START_X 0
 #define START_Y 0
 
@@ -46,12 +49,14 @@ int main()
 
 	if (buscarTesoro(laberinto, &sol_x, &sol_y, START_X, START_Y)) // Completar parámetros
 	{
+        cout << "\n" << endl;
         imprimirLaberinto(laberinto);
-        cout << "ENCONTRADO " << sol_x << " " << sol_y << endl;
+		cout << "ENCONTRADO " << sol_x << " " << sol_y << endl;
 	}
 	else
     {
-		cout << "INALCANZABLE" << '\n';
+        imprimirLaberinto(laberinto);
+		cout << "INALCANZABLE" << endl;
     }
 
 	return 0;
@@ -82,55 +87,50 @@ int main()
         - Objetivo 'T'
         - Backtrack
 */
-bool buscarTesoro(char **laberinto, int *sol_x, int *sol_y, int x, int y) // Completar parámetros
+bool buscarTesoro(char **laberinto, int *sol_x, int *sol_y, int x, int y)
 {
-    // cout << "\n" << endl;
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = { 0, 1, 0,-1};
 
-    bool encontrado = false;
-
-    //            ☝️  👉  👇  👈
-    int dx[4] = { -1,  0,  1,  0};
-    int dy[4] = {  0,  1,  0, -1};
-    int nx;
-    int ny;
-
-    laberinto[x][y] = 'X';
-    // imprimirLaberinto(laberinto); // todo test
-
-    // Procesamiento + Llamada recursiva
-    for(int i = 0; i < 4; i++)
+    // Marcar la casilla actual como parte del camino provisional
+    if (laberinto[x][y] == '.')
     {
-        nx = x + dx[i];
-        ny = y + dy[i];
-
-        // Poda
-            // Comprobamos x e y Dentro del tablero
-            // Comprobamos que no es pared y no es visitado
-        if(
-            (nx >= 0 && nx < TAM) && (ny >= 0 && ny < TAM) &&
-            (laberinto[nx][ny] != '*' && laberinto[nx][ny] != 'X'))
-        {
-            // Caso Valido y caso objetivo
-            switch (laberinto[nx][ny])
-            {
-                case '.':
-                    encontrado = buscarTesoro(laberinto, sol_x, sol_y, nx, ny);
-                    break;
-                case 'T':
-                    encontrado = true;
-                    *sol_x = nx;
-                    *sol_y = ny;
-                    return encontrado;
-                    break;
-            }
-        }
-        if(encontrado)
-        {
-            break;
-        };
+        laberinto[x][y] = 'X';
     }
 
-    return encontrado;
+    for (int i = 0; i < 4; i++)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        // Poda
+        if (nx >= 0 && nx < TAM &&
+            ny >= 0 && ny < TAM &&
+            laberinto[nx][ny] != '*' &&
+            laberinto[nx][ny] != 'X')
+        {
+            // Si encontramos el tesoro, guardamos coordenadas y mantenemos el camino
+            if (laberinto[nx][ny] == 'T')
+            {
+                *sol_x = nx;
+                *sol_y = ny;
+                return true;
+            }
+
+            // Si es una casilla libre, seguimos explorando
+            if (laberinto[nx][ny] == '.')
+            {
+                if (buscarTesoro(laberinto, sol_x, sol_y, nx, ny))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+
+    // Si ninguna dirección funciona, esta casilla no forma parte del camino correcto
+    laberinto[x][y] = '.';
+    return false;
 }
 
 // Funciones auxiliares

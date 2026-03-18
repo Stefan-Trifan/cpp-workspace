7e2854f8f9a41ff2c13dfbcc35638adc346d5afa
Stefan <trfstephan@gmail.com>
2026-03-18 11:17:52 +0100

    update

--- 02_utad_evaluables/5a_laberinto/main.cpp
+++ 02_utad_evaluables/5a_laberinto/main.cpp
@@ -9,22 +9,53 @@
    Inicio cabecera */
 
 #include <iostream>
+// clang-format off
+#define assertdomjudge(x) if(!(x)){std::cout<<"ERROR"<<std::endl;exit(0);}
+// clang-format on
+
+#define TAM 10
+#define START_X 0
+#define START_Y 0
+
 using namespace std;
 
 // Funciones del programa
+bool buscarTesoro(char **laberinto, int *sol_x, int *sol_y, int x, int y); // Completar parámetros
 
 // Funciones auxiliares
+void imprimirLaberinto(char **laberinto);
 
 /* _________________________________________
    Inicio main() */
 
 int main()
 {
-	cout << "\n________________________________START\n\n";
-
 	// Declaración de variables
+	int sol_x = 0, sol_y = 0;
+	char **laberinto;
+	laberinto = new char *[TAM];
 
-	cout << "\n________________________________EXIT\n\n";
+    // Pide laberinto por consola, reserva memoria
+	for (int i = 0; i < TAM; i++)
+	{
+		laberinto[i] = new char[TAM];
+		for (int j = 0; j < TAM; j++)
+        {
+			cin >> laberinto[i][j];
+        }
+	}
+
+	if (buscarTesoro(laberinto, &sol_x, &sol_y, START_X, START_Y)) // Completar parámetros
+	{
+        imprimirLaberinto(laberinto);
+		cout << "ENCONTRADO " << sol_x << " " << sol_y << endl;
+	}
+	else
+    {
+        imprimirLaberinto(laberinto);
+		cout << "INALCANZABLE" << endl;
+    }
+
 	return 0;
 }
 
@@ -32,5 +63,85 @@ int main()
    Inicio definicion de funciones */
 
 // Funciones del programa
+/*
+    Elementos necesarios
+        - Coordenadas actuales 'x', 'y'
+        - Caminos Posibles: 'dx' y 'dy'
+
+                        ( x  y)
+
+            - Arriba    (-1, 0)
+            - Derecha   ( 0, 1)
+            - Abajo     ( 1, 0)
+            - Izquierda ( 0,-1)
+
+    Condiciones de estado
+        - Poda:
+            - Fuera
+            - Pared '*'
+            - Visitado 'X'
+        - Válido '.'
+        - Objetivo 'T'
+        - Backtrack
+*/
+bool buscarTesoro(char **laberinto, int *sol_x, int *sol_y, int x, int y) // Completar parámetros
+{
+    cout << "\n" << endl;
+    imprimirLaberinto(laberinto); // todo test
+
+    //            ☝️  👉  👇  👈
+    int dx[4] = { -1,  0,  1,  0};
+    int dy[4] = {  0,  1,  0, -1};
+    int nx;
+    int ny;
+
+    // Procesamiento + Llamada recursiva
+    for(int i = 0; i < 4; i++)
+    {
+        nx = x + dx[i];
+        ny = y + dy[i];
+
+        // Poda
+            // Comprobamos x e y Dentro del tablero
+            // Comprobamos que no es pared y no es visitado
+        if(
+            (nx >= 0 && nx < TAM) && (ny >= 0 && ny < TAM) &&
+            (laberinto[nx][ny] != '*' && laberinto[nx][ny] != 'X'))
+        {
+            laberinto[x][y] = 'X';
+
+            // Caso Valido y caso objetivo
+            switch (laberinto[nx][ny])
+            {
+                case '.':
+                    return buscarTesoro(laberinto, sol_x, sol_y, nx, ny);
+                    break;
+                case 'T':
+                    *sol_x = nx;
+                    *sol_y = ny;
+                    return true;
+                    break;
+            }
+        }
+    }
+
+    laberinto[x][y] = 'X';
+    // nx = x - dx[i];
+    // ny = y - dy[i];
+    // return buscarTesoro(laberinto, sol_x, sol_y, x, y);
+
+    return false; // todo testing
+}
 
 // Funciones auxiliares
+void imprimirLaberinto(char **laberinto)
+{
+	for (int i = 0; i < TAM; i++)
+	{
+		for (int j = 0; j < TAM; j++)
+        {
+			cout << laberinto[i][j];
+        }
+		cout << endl;
+	}
+}

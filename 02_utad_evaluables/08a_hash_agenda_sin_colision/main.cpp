0f6cc17c51731ff9b921a4626a26f26001780c7a
Stefan <trfstephan@gmail.com>
2026-04-10 10:06:38 +0200

    update

---
+++ 02_utad_evaluables/08a_hash_agenda_sin_colision/main.cpp
@@ -0,0 +1,36 @@
+/*
+    Stefan Trifan
+
+    #if 0
+    #en
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <iostream>
+using namespace std;
+// clang-format off
+#define assertdomjudge(x) if(!(x)){std::cout<<"ERROR"<<std::endl;exit(0);}
+// clang-format on
+
+// Funciones del programa
+
+// Funciones auxiliares
+
+/* _________________________________________
+   Inicio main() */
+
+int main()
+{
+	// Declaración de variables
+
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+// Funciones del programa
+
+// Funciones auxiliares

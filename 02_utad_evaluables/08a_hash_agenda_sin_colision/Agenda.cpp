5569d131b65aae0d01470e3e634af0f91fd8c995
Stefan <trfstephan@gmail.com>
2026-04-15 17:44:00 +0200

    update

--- 02_utad_evaluables/08a_hash_agenda_sin_colision/Agenda.cpp
+++ 02_utad_evaluables/08a_hash_agenda_sin_colision/Agenda.cpp
@@ -1,43 +1,71 @@
 #include "Agenda.h"
 #include "assertdomjudge.h"
-#include "iostream"
+#include <iostream>
 
 using namespace std;
 
-// Constructores
+// _________________________________________ Constructores
+/** @brief
+ * Inicializará el atributo capacidad y reservará capacidad elementos para todos los arrays
+ * */
 Agenda::Agenda(int capacidad)
 {
 
 }
 
-// Getter
+/** @brief
+ * Destructor. Se encargará de liberar la memoria que fue reservada de forma dinámica para almacenar todos los arrays.
+ * */
+Agenda::~Agenda()
+{
+
+}
+
+// _________________________________________ Getter
+/** @brief
+ * Devuelve el nombre del contacto que tiene el teléfono indicado.
+ * */
 string Agenda::getContacto(long telefono)
 {
 
 }
 
-// Métodos públicos
+// _________________________________________ Métodos públicos
+/** @brief
+ * Este método implementa la función hash que permite obtener la posición que corresponde al número de teléfono indicado dentro de la tabla hash. La función hash estará basada en el resto de la división entre la capacidad.
+ * */
 int Agenda::obtenerPosicion(long telefono)
 {
 
 }
 
+/** @brief
+ * Devuelve si el contacto con el teléfono indicado está almacenado o no en la tabla hash.
+ * */
 bool Agenda::existeContacto(long telefono)
 {
 
 }
 
-
+/** @brief
+ * Introduce un contacto nuevo en la tabla hash en su posición correspondiente.
+ * */
 void Agenda::introducirContacto(long telefono, string contacto)
 {
 
 }
 
+/** @brief
+ * Elimina el contacto con el teléfono indicado de la tabla hash.
+ * */
 void Agenda::eliminarContacto(long telefono)
 {
 
 }
 
+/** @brief
+ * Esta función permite imprimir toda la tabla hash. Aunque este método viola el principio de separación entre interfaz y modelo es necesario para el corrector automático. Esta función no deberá ser implementada por el alumno.
+ * */
 // clang-format off
 void Agenda::imprimir()
 {
--- 02_utad_evaluables/08a_hash_agenda_sin_colision/Agenda.h
+++ 02_utad_evaluables/08a_hash_agenda_sin_colision/Agenda.h
@@ -3,20 +3,34 @@
 class Agenda
 {
 	private:
-		// atributo que indica el máximo número de contactos que se pueden almacenar en la tabla hash.
+		/**
+         * Num max de contactos que se pueden almacenar
+         * en la tabla hash. */
 		int capacidad;
 
-		// Puntero a un array de string que permitirá almacenar los nombres de los contactos.Este array tendrá capacidad elementos.
+		/**
+         * Puntero a un array de string
+         * que permitirá almacenar los nombres de los contactos.
+         * Este array tendrá capacidad elementos. */
 		string* nombres;
 
-		// Puntero a un array de long que permitirá almacenar los teléfonos de los contactos.Este array tendrá capacidad elementos.
+		/**
+         * Puntero a un array de long
+         * que permitirá almacenar los teléfonos de los contactos.
+         * Este array tendrá capacidad elementos. */
         long* telefonos;
 
-		// Puntero a un array de booleanos que permite indicar que posiciones de la tabla hash están ocupadas.Este array tendrá capacidad elementos.Si una posición esta vacia se considerará que sus valores asociados son basura.
+		/**
+         * Puntero a un array de booleanos
+         * que permite indicar que posiciones
+         * de la tabla hash están ocupadas.
+         * Este array tendrá capacidad elementos.
+         * Si una posición esta vacia se considerará que sus valores asociados son basura. */
         bool* ocupados;
 
 	public:
 		Agenda(int capacidad);
+        ~Agenda();
 		string getContacto(long telefono);
 		int obtenerPosicion(long telefono);
 		bool existeContacto(long telefono);

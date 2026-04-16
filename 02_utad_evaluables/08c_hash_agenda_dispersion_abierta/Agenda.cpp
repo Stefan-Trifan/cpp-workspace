e32325377767bfd597468acf30cfa0eec6798f2c
Stefan <trfstephan@gmail.com>
2026-04-16 17:31:52 +0200

    update

--- 02_utad_evaluables/08c_hash_agenda_dispersion_abierta/Agenda.cpp
+++ 02_utad_evaluables/08c_hash_agenda_dispersion_abierta/Agenda.cpp
@@ -12,7 +12,11 @@ using namespace std;
 
 /** @brief
  * - Inicializará el atributo [capacidad]
- * - Reservará [capacidad] listas para la tabla hash. */
+ * - Reservará [capacidad] listas para la tabla hash. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 Agenda::Agenda(int capacidad)
 {
 }
@@ -21,7 +25,11 @@ Agenda::Agenda(int capacidad)
  * - Destructor.
  * - Se encargará de liberar la memoria
  * que fue reservada de forma dinámica
- * para almacenar toda la tabla hash. */
+ * para almacenar toda la tabla hash. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 Agenda::~Agenda()
 {
 }
@@ -31,7 +39,11 @@ Agenda::~Agenda()
  * obtener en que posición del vector de listas enlazadas
  * que corresponde al número de teléfono indicado
  * dentro de la tabla hash.
- * - La función hash estará basada en el resto de la división entre la capacidad. */
+ * - La función hash estará basada en el resto de la división entre la capacidad. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 int Agenda::obtenerPosicion(long telefono)
 {
 	return 0;
@@ -39,7 +51,11 @@ int Agenda::obtenerPosicion(long telefono)
 
 /** @brief
  * - Devuelve si el contacto con el teléfono indicado
- * está almacenado o no en la tabla hash. */
+ * está almacenado o no en la tabla hash. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 bool Agenda::existeContacto(long telefono)
 {
 	return false;
@@ -47,7 +63,11 @@ bool Agenda::existeContacto(long telefono)
 
 /** @brief
  * - Devuelve el nombre del contacto
- * que tiene el teléfono indicado. */
+ * que tiene el teléfono indicado. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 string Agenda::getContacto(long telefono)
 {
 	return "";
@@ -55,14 +75,22 @@ string Agenda::getContacto(long telefono)
 
 /** @brief
  * - Introduce un contacto nuevo en la tabla hash en la
- * lista que se encuentra en la posición correspondiente. */
+ * lista que se encuentra en la posición correspondiente. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 void Agenda::introducirContacto(long telefono, string contacto)
 {
 }
 
 /** @brief
  * - Elimina el contacto con el teléfono
- * indicado de la tabla hash. */
+ * indicado de la tabla hash. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 void Agenda::eliminarContacto(long telefono)
 {
 }
@@ -72,7 +100,11 @@ void Agenda::eliminarContacto(long telefono)
  * - Aunque este método viola el principio de separación
  * entre interfaz y modelo es necesario
  * para el corrector automático. Esta función no deberá
- * ser implementada por el alumno. */
+ * ser implementada por el alumno. 
+ * 
+ * Espacial:
+ * Temporal:
+ * */
 void Agenda::imprimir()
 {
 	for (int i = 0; i < capacidad; i++)

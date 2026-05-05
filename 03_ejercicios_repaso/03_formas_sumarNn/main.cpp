bafcffb7b9ff8caa1d28b445570c483122c3c5bc
Stefan <trfstephan@gmail.com>
2026-05-05 15:22:34 +0200

    update

---
+++ 03_ejercicios_repaso/03_formas_sumarNn/main.cpp
@@ -0,0 +1,40 @@
+/*
+    Stefan Trifan
+
+    #if 0
+    #en
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <iostream>
+
+using namespace std;
+// clang-format off
+#define assertdomjudge(x) if(!(x)){std::cout<<"ERROR"<<std::endl;exit(0);}
+// clang-format on
+
+// Funciones del programa
+
+// Funciones auxiliares
+
+/* _________________________________________
+   Inicio main() */
+
+int main()
+{
+    cout << "\n_________________________________________START\n\n";
+    
+	// Declaración de variables
+
+    cout << "\n_________________________________________EXIT\n\n";
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+// Funciones del programa
+
+// Funciones auxiliares
